fe1fb78b8965496bd242f0903046bb8cac75679f
Lavergne <hugo.lavergne@devinci.fr>
2020-02-09 01:20:03 +0100

    Video entity & Fixtures for Videos

--- src/DataFixtures/VideoFixtures.php
+++ src/DataFixtures/VideoFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\User;
 use App\Entity\Video;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -24,6 +25,77 @@ class VideoFixtures extends Fixture
             $manager->persist($video);
         }
         $manager->flush();
+
+        $this->loadLikes($manager);
+        $this->loadDislikes($manager);
+
+
+    }
+
+    public function loadLikes($manager)
+    {
+        foreach ($this->likesData() as [$video_id, $user_id])
+        {
+            $video = $manager->getRepository(Video::class)->find($video_id);
+            $user = $manager->getRepository(User::class)->find($user_id);
+
+            $video->addUsersThatLike($user);
+            $manager->persist($video);
+        }
+        $manager->flush();
+    }
+
+
+    public function loadDislikes($manager)
+    {
+        foreach ($this->dislikesData() as [$video_id, $user_id])
+        {
+            $video = $manager->getRepository(Video::class)->find($video_id);
+            $user = $manager->getRepository(User::class)->find($user_id);
+
+            $video->addUsersThatDontLike($user);
+            $manager->persist($video);
+        }
+        $manager->flush();
+    }
+
+
+    private function likesData()
+    {
+        return [
+            [12, 1],
+            [12, 2],
+            [12, 3],
+
+            [11, 1],
+            [11, 2],
+
+            [1, 1],
+            [1, 2],
+            [1, 3],
+
+            [2, 1],
+            [2, 2],
+        ];
+    }
+
+    private function dislikesData()
+    {
+        return [
+            [10, 1],
+            [10, 2],
+            [10, 3],
+
+            [11, 1],
+            [11, 2],
+
+            [1, 1],
+            [1, 2],
+            [1, 3],
+
+            [2, 1],
+            [2, 2],
+        ];
     }
 
     private function VideoData()
--- src/Migrations/Version20200209001742.php
+++ src/Migrations/Version20200209001742.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200202151733 extends AbstractMigration
+final class Version20200209001742 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,13 +23,19 @@ final class Version20200202151733 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, parent_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_3AF34668727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE videos (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, path VARCHAR(255) NOT NULL, duration INT DEFAULT NULL, INDEX IDX_29AA643212469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE likes (video_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_49CA4E7D29C1004E (video_id), INDEX IDX_49CA4E7DA76ED395 (user_id), PRIMARY KEY(video_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE dislikes (video_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_2DF3BE1129C1004E (video_id), INDEX IDX_2DF3BE11A76ED395 (user_id), PRIMARY KEY(video_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, video_id INT NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_5F9E962AA76ED395 (user_id), INDEX IDX_5F9E962A29C1004E (video_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(45) NOT NULL, last_name VARCHAR(45) NOT NULL, vimeo_api_key VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE videos (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, path VARCHAR(255) NOT NULL, duration INT DEFAULT NULL, INDEX IDX_29AA643212469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_3AF34668727ACA70 FOREIGN KEY (parent_id) REFERENCES categories (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE videos ADD CONSTRAINT FK_29AA643212469DE2 FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE likes ADD CONSTRAINT FK_49CA4E7D29C1004E FOREIGN KEY (video_id) REFERENCES videos (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE likes ADD CONSTRAINT FK_49CA4E7DA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE dislikes ADD CONSTRAINT FK_2DF3BE1129C1004E FOREIGN KEY (video_id) REFERENCES videos (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE dislikes ADD CONSTRAINT FK_2DF3BE11A76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962AA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962A29C1004E FOREIGN KEY (video_id) REFERENCES videos (id)');
-        $this->addSql('ALTER TABLE videos ADD CONSTRAINT FK_29AA643212469DE2 FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema) : void
@@ -39,11 +45,17 @@ final class Version20200202151733 extends AbstractMigration
 
         $this->addSql('ALTER TABLE categories DROP FOREIGN KEY FK_3AF34668727ACA70');
         $this->addSql('ALTER TABLE videos DROP FOREIGN KEY FK_29AA643212469DE2');
-        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962AA76ED395');
+        $this->addSql('ALTER TABLE likes DROP FOREIGN KEY FK_49CA4E7D29C1004E');
+        $this->addSql('ALTER TABLE dislikes DROP FOREIGN KEY FK_2DF3BE1129C1004E');
         $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962A29C1004E');
+        $this->addSql('ALTER TABLE likes DROP FOREIGN KEY FK_49CA4E7DA76ED395');
+        $this->addSql('ALTER TABLE dislikes DROP FOREIGN KEY FK_2DF3BE11A76ED395');
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962AA76ED395');
         $this->addSql('DROP TABLE categories');
+        $this->addSql('DROP TABLE videos');
+        $this->addSql('DROP TABLE likes');
+        $this->addSql('DROP TABLE dislikes');
         $this->addSql('DROP TABLE comments');
         $this->addSql('DROP TABLE users');
-        $this->addSql('DROP TABLE videos');
     }
 }
